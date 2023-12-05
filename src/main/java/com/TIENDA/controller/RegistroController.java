8c0dc0f54c32681ca44c2516541a2b81069994a2
¨Jenny¨ <jgong10278@ufide.ac.cr>
2023-12-05 15:50:34 -0600

    Reportes

--- src/main/java/com/TIENDA/controller/RegistroController.java
+++ src/main/java/com/TIENDA/controller/RegistroController.java
@@ -1,24 +1,70 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/springframework/Controller.java to edit this template
- */
+
 package com.TIENDA.controller;
 
+import com.TIENDA.domain.Usuario;
+import com.TIENDA.service.RegistroService;
+import jakarta.mail.MessagingException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
-/**
- *
- * @author kembr
- */
 @Controller
+@Slf4j
+@RequestMapping("/registro")
 public class RegistroController {
-    
-    @RequestMapping("/url")
-    public String page(Model model) {
-        model.addAttribute("attribute", "value");
-        return "view.name";
+
+    @Autowired
+    private RegistroService registroService;
+
+    @GetMapping("/nuevo")
+    public String nuevo(Model model, Usuario usuario) {
+        return "/registro/nuevo";
     }
-    
+
+    @GetMapping("/recordar")
+    public String recordar(Model model, Usuario usuario) {
+        return "/registro/recordar";
+    }
+
+    @PostMapping("/crearUsuario")
+    public String crearUsuario(Model model, Usuario usuario) 
+            throws MessagingException {
+        model = registroService.crearUsuario(model, usuario);
+        return "/registro/salida";
+    }
+
+    @GetMapping("/activacion/{usuario}/{id}")
+    public String activar(
+            Model model, 
+            @PathVariable(value = "usuario") String usuario, 
+            @PathVariable(value = "id") String id) {
+        model = registroService.activar(model, usuario, id);
+        if (model.containsAttribute("usuario")) {
+            return "/registro/activa";
+        } else {
+            return "/registro/salida";
+        }
+    }
+
+    @PostMapping("/activar")
+    public String activar(
+            Usuario usuario, 
+            @RequestParam("imagenFile") MultipartFile imagenFile) {
+        registroService.activar(usuario, imagenFile);
+        return "redirect:/";
+    }
+
+    @PostMapping("/recordarUsuario")
+    public String recordarUsuario(Model model, Usuario usuario) 
+            throws MessagingException {
+        model = registroService.recordarUsuario(model, usuario);
+        return "/registro/salida";
+    }
 }
